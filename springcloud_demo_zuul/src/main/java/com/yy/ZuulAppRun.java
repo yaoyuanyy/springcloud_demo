96fc8235c510fe189fc7937a15e09d6c5aac4e8d
yaoyuanyy <yaoliang_yl_ok@163.com>
2019-01-05 21:35:30 +0800

    update zuul

--- springcloud_demo_zuul/src/main/java/com/yy/ZuulAppRun.java
+++ springcloud_demo_zuul/src/main/java/com/yy/ZuulAppRun.java
@@ -1,16 +1,17 @@
 package com.yy;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
-@SpringCloudApplication
+@SpringBootApplication
 @EnableZuulProxy
-public class AppRun {
+public class ZuulAppRun {
 
 	public static void main(String[] args) {
-        new SpringApplicationBuilder(AppRun.class).web(true).run(args);
+        new SpringApplicationBuilder(ZuulAppRun.class).web(true).run(args);
 	}
 
 }
